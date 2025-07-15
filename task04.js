e212fca9ad91e2354ff4d9d70e7261d2f2d4a07f
watcharap <watchara.paramee@gmail.com>
2025-07-15 13:49:47 +0700

    task04 solution

--- task04.js
+++ task04.js
@@ -8,4 +8,24 @@
 
 // TODO BONUS: Rewrite with if
 
-const num = 15;
+const num = 2;
+
+if (num%3 === 0 && num%5 === 0) {console.log("Fizzbuzz") }
+    
+else if(num%3 === 0) {console.log ("Fizz")}
+else if(num%5 === 0) {console.log ("Buzz") }
+else {console.log ("Not divisible by 3 or 5") }
+
+// TODO BONUS: Rewrite with if
+
+let result = "";
+if (num % 3 === 0) {
+  result += "Fizz";
+}
+if (num % 5 === 0) {
+  result += "Buzz";
+}
+if (result === "") {
+  result = "Not divisible by 3 or 5";
+}
+console.log(result);
